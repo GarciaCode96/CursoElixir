47650d4632da518eff48b8697d77060180b31010
Jesus Alejandro Garcia <garciacode96@gmail.com>
2019-03-06 03:19:06 -0600

    creacion de primer proyecto en Elixir

---
+++ lib/practica.ex
@@ -0,0 +1,18 @@
+defmodule Practica do
+  @moduledoc """
+  Documentation for Practica.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Practica.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
